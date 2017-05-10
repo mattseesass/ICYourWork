84781844bdad2c04691fc1608252a5e6f0b22dee
Robin <Robin.vriens@outlook.com>
2017-05-10 18:18:59 +0200

    Updated Index
    Added an article and changed some css

--- ICYourWork/index.php
+++ ICYourWork/index.php
@@ -1,16 +1,100 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
 <meta charset="UTF-8">
 <title>ICYourWork</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
 </head>
-
 <body>
+	<div id="header-div">
+	</div>
 
+	<div id="main-wrap">
+		<article>
+			<div class="article-profile">
+				<div class="company-picture">
+					<img src="core/img/profile.png">
+				</div>
+				<div class="user-profile">
+					<div class="work-picture">
+						<img src="core/img/profile.png">
+					</div>
+					<div class="profile-username">
+						<span>Testuser123</span>
+					</div>
+				</div>
+			</div>
+			<div class="article-title">
+				<div class="company-slogan">
+					<span>company slogan test<span>
+				</div>
+					<span class="company-name">Company name<span>
+			</div>
+			<div class="article-picture">
+				<img src="">
+			</div>
+			<div class="article-widgets">
+				<div class="acrticle-likes">
+					5665 Likes
+				</div>
+				<div class="acrticle-comments">
+					147 Comments
+				</div>
+				<div class="acrticle-reviews">
+					12 Reviews
+				</div>	
+			</div>
+			<div class="article-social">
+				<div class="btn-share" id="facebook"><i class="fa fa-facebook"></i>Facebook</div>
+				<div class="btn-share" id="twitter"><i class="fa fa-twitter"></i>Twitter</div>
+				<div class="btn-share" id="google"><i class="fa fa-google"></i>Google +</div>
+			</div>	
+		</article>
+		<article>
+			<div class="article-profile">
+				<div class="company-picture">
+					<img src="core/img/profile.png">
+				</div>
+				<div class="user-profile">
+					<div class="work-picture">
+						<img src="core/img/profile.png">
+					</div>
+					<div class="profile-username">
+						<span>Testuser123</span>
+					</div>
+				</div>
+			</div>
+			<div class="article-title">
+				<div class="company-slogan">
+					<span>company slogan test<span>
+				</div>
+					<span class="company-name">Company name<span>
+			</div>
+			<div class="article-picture">
+				<img src="">
+			</div>
+			<div class="article-widgets">
+				<div class="acrticle-likes">
+					5665 Likes
+				</div>
+				<div class="acrticle-comments">
+					147 Comments
+				</div>
+				<div class="acrticle-reviews">
+					12 Reviews
+				</div>	
+			</div>
+			<div class="article-social">
+				<div class="btn-vote"><i class="fa fa-arrow-circle-up"></i></div>
+				<div class="btn-vote"><i class="fa fa-comments"></i></div>
+				<div class="btn-share" id="facebook"><i class="fa fa-facebook"></i>Facebook</div>
+				<div class="btn-share" id="twitter"><i class="fa fa-twitter"></i>Twitter</div>
+				<div class="btn-share" id="google"><i class="fa fa-google"></i>Google +</div>
+			</div>	
+		</article>
+	</div>
 </body>
-
 </html>
